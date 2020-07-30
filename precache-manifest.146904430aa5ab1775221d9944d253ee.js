b87e8ed1a844e58d5a0b2518fafe9f213862bde1
Andrew <ngandrew@umich.edu>
2020-07-30 14:38:41 -0400

    Updates

---
+++ precache-manifest.146904430aa5ab1775221d9944d253ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a99ae9f7f1e373f0538ab77edbd4e43",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "18dfbd494b3c0165e705",
+    "url": "/portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e44729af9527b5886190",
+    "url": "/portfolio/static/js/2.2494d31c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.2494d31c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18dfbd494b3c0165e705",
+    "url": "/portfolio/static/js/main.48698a23.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
