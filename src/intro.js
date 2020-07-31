68b4b145249be0ef6cd0b587c6b9f256d423c499
Andrew <ngandrew@umich.edu>
2020-07-31 16:45:11 -0400

    ok

--- src/intro.js
+++ src/intro.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Intro() {
+function Intro(props) {
+    const { className } = props;
+
     return (
       <div>
-        <h1>Hello. I&apos;m Andrew.</h1>
+        <h1 className={className}>Hello. I&apos;m Andrew.</h1>
       </div>
     )
+}
+
+Intro.propTypes = {
+    className: PropTypes.string
+}
+
+Intro.defaultProps = {
+    className: ''
 }
 
 export default Intro;
