93f15c331b550d7fc8ddf5279f2b2d3ec69e2775
Andrew <ngandrew@umich.edu>
2020-08-01 12:33:09 -0400

    updates

--- src/portfolio.js
+++ src/portfolio.js
@@ -7,69 +7,91 @@ const HEADER_OPTIONS = ['Welcome', 'About', 'Experience', 'Resume', 'Contact']
 
 class Portfolio extends React.Component {
     constructor(props) {
-        super(props);
+      super(props);
 
-        this.state = {
-            theme: 'light',
-            activeView: 'Welcome'
-        };
-    }
+      this.state = {
+          theme: 'light',
+          activeView: 'Welcome'
+      };
+  }
 
     changeTheme = () => {
-        const { theme } = this.state;
+      const { theme } = this.state;
 
-        if (theme === 'light') {
-            this.setState({ theme: 'dark' });
-        } else {
-            this.setState({ theme: 'light' });
-        }
+      if (theme === 'light') {
+          this.setState({ theme: 'dark' });
+      } else {
+          this.setState({ theme: 'light' });
+      }
     };
 
     changeView = (e) => {
-        this.setState({ activeView: e.target.value });
+      this.setState({ activeView: e.target.value });
     }
 
-    renderHeader = () => {
-        const { theme } = this.state;
+    renderHeader() {
+      const { theme } = this.state;
 
-        const changeThemeTo = theme === 'light' ? 'Dark Mode' : 'Light Mode';
+      const changeThemeTo = theme === 'light' ? 'Dark Mode' : 'Light Mode';
 
-        return (
-          <div>
-            {
-              HEADER_OPTIONS.map((item, i) => (
-                // eslint-disable-next-line react/no-array-index-key
-                <li key={i}>
-                  <button
-                    type="button"
-                    value={item}
-                    onClick={this.changeView}
-                  >
-                    {item}
-                  </button>
-                </li>
-              ))
-            }
-            <button
-              id="theme-button"
-              type="button"
-              onClick={this.changeTheme}
-            >
-              { changeThemeTo }
-            </button>
-          </div>
-        );
+      return (
+        <div>
+          {
+            HEADER_OPTIONS.map((item, i) => (
+              // eslint-disable-next-line react/no-array-index-key
+              <li key={i}>
+                <button
+                  type="button"
+                  value={item}
+                  onClick={this.changeView}
+                >
+                  {item}
+                </button>
+              </li>
+            ))
+          }
+          <button
+            id="theme-button"
+            type="button"
+            onClick={this.changeTheme}
+          >
+            { changeThemeTo }
+          </button>
+        </div>
+      );
+    }
+
+    renderView() {
+      const { activeView } = this.state;
+
+      let view;
+
+      switch(activeView) {
+        case 'About':
+          break;
+        case 'Experience':
+          break;
+        case 'Resume':
+          break;
+        case 'Contact':
+          break;
+        default:
+          view = <Intro className="intro" />;
+          break;
+      }
+
+      return view;
     }
 
     render() {
-        const { theme } = this.state;
+      const { theme } = this.state;
 
-        return (
-          <div className={classNames('portfolio', theme)}>
-            {this.renderHeader}
-            <Intro className="intro" />
-          </div>
-        );
+      return (
+        <div className={classNames('portfolio', theme)}>
+          {this.renderHeader}
+          {this.renderView}
+        </div>
+      );
     }
 }
 
