f248fbda2401b3b4fc5d47d2385d4fd815534c63
Andrew <ngandrew@umich.edu>
2020-08-01 23:47:04 -0400

    added files

--- src/welcome.js
+++ src/welcome.js
@@ -8,6 +8,7 @@ function Intro(props) {
     return (
       <div>
         <h1 className={className}>Hello. I&apos;m Andrew.</h1>
+        <img src='./mainpic.jpg' alt='My pic' />
       </div>
     )
 }
