01f708feede5b9ae7b9b22246ae6812e16ff5aa2
Andrew <ngandrew@umich.edu>
2020-08-02 12:34:10 -0400

    Updates

--- precache-manifest.59bdb440d773112fa00d7835361bd920.js
+++ precache-manifest.59bdb440d773112fa00d7835361bd920.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0e0ed2a81ab281836a5f9c8823d4cf99",
+    "revision": "018bb94a1a9f6b7ae96173a199c27adf",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "c50591b6596c1a3743fc",
+    "revision": "1644814b8fd2c0f0dbbb",
     "url": "/portfolio/static/css/main.ce3484f0.chunk.css"
   },
   {
-    "revision": "fac471277dd4db60e661",
-    "url": "/portfolio/static/js/2.a79d24ac.chunk.js"
+    "revision": "d49ac3ceade4d11dc5a5",
+    "url": "/portfolio/static/js/2.1b328b47.chunk.js"
   },
   {
     "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
-    "url": "/portfolio/static/js/2.a79d24ac.chunk.js.LICENSE.txt"
+    "url": "/portfolio/static/js/2.1b328b47.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c50591b6596c1a3743fc",
-    "url": "/portfolio/static/js/main.c046c270.chunk.js"
+    "revision": "1644814b8fd2c0f0dbbb",
+    "url": "/portfolio/static/js/main.85282fcc.chunk.js"
   },
   {
     "revision": "7a6cc99bd8f08f9fa6ec",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.474d4d100e743b742982d550c5508400.js"
+  "/portfolio/precache-manifest.59bdb440d773112fa00d7835361bd920.js"
 );
 
 self.addEventListener('message', (event) => {
