db6eb2657aa5ebb3a22dff66400b8d2fe5950328
Andrew <ngandrew@umich.edu>
2020-08-02 14:10:45 -0400

    updates

--- src/about.js
+++ src/about.js
@@ -9,41 +9,45 @@ class About extends React.Component {
     }
 
     render() {
-        return (
-          <div id="about">
-            <img id="my-pic" src="mainpic.jpg" alt='My pic' />
-            <p>
-              Hello! It&apos;s great to meet you. I&apos;m Andrew, a junior studying computer science at the University of Michigan. 
-              My passion for computer science and programming actually started when I took my very first programming course, and 
-              fell in love with it! Through my time in school, I have taken a wide variety of interesting CS courses, including 
-              those learning about different data structures and algorithms, and one where I got to work with python and Spotify API 
-              data to create unique data visualizations. Funny enough however, my favorite class I&apos;ve taken had nothing to do 
-              with CS, and was about classic civilizations!
-            </p>
-            <p>
-              Outside of my classes, I&apos;ve been involved in numerous student organizations on campus, including a few of my favorites,
-              the Alternate Reality Initiative, where I got to work extensively with UX and virtual reality, the Michigan Business Club,
-              where I was able to expand my interests in learning about different aspects of business, and of course, the time I got to 
-              work as a research assistant studying behavioral game theory! 
-            </p>
-            <p>
-              Through my experiences at school, I&apos;ve gained a deep interests in more theoretical fields of technology such as 
-              machine learning and artifical intelligence. I&apos;ve also developed strong interests in virtual reality as well 
-              as front-end web development, where I get to work on desiging strong user experiences. 
-            </p>
-            <p>
-              When I&apos;m not working, I like to spend time with healthy practices like running and working out, as well as reading 
-              and meditating. Of course, I have quite a strong sweet tooth, and enjoy baking as well. On the more creative side,
-              I&apos;ve recently taken up drawing! 
-            </p>
-            <p>
-              Feel free to ask me about any of my interests, technical or otherwise! 
-            </p>
-            <p>
-              -Andrew
-            </p>
-          </div> 
-        )
+      const { visitor } = this.props;
+
+      return (
+        <div id="about">
+          <img id="my-pic" src="mainpic.jpg" alt='My pic' />
+          <p>
+            Hello, 
+            {visitor}
+            ! It&apos;s great to meet you. I&apos;m Andrew, a junior studying computer science at the University of Michigan. 
+            My passion for computer science and programming actually started when I took my very first programming course, and 
+            fell in love with it! Through my time in school, I have taken a wide variety of interesting CS courses, including 
+            those learning about different data structures and algorithms, and one where I got to work with python and Spotify API 
+            data to create unique data visualizations. Funny enough however, my favorite class I&apos;ve taken had nothing to do 
+            with CS, and was about classic civilizations!
+          </p>
+          <p>
+            Outside of my classes, I&apos;ve been involved in numerous student organizations on campus, including a few of my favorites,
+            the Alternate Reality Initiative, where I got to work extensively with UX and virtual reality, the Michigan Business Club,
+            where I was able to expand my interests in learning about different aspects of business, and of course, the time I got to 
+            work as a research assistant studying behavioral game theory! 
+          </p>
+          <p>
+            Through my experiences at school, I&apos;ve gained a deep interests in more theoretical fields of technology such as 
+            machine learning and artifical intelligence. I&apos;ve also developed strong interests in virtual reality as well 
+            as front-end web development, where I get to work on desiging strong user experiences. 
+          </p>
+          <p>
+            When I&apos;m not working, I like to spend time with healthy practices like running and working out, as well as reading 
+            and meditating. Of course, I have quite a strong sweet tooth, and enjoy baking as well. On the more creative side,
+            I&apos;ve recently taken up drawing! 
+          </p>
+          <p>
+            Feel free to ask me about any of my interests, technical or otherwise! 
+          </p>
+          <p>
+            -Andrew
+          </p>
+        </div> 
+      );
     }
 }
 
--- src/portfolio.js
+++ src/portfolio.js
@@ -2,7 +2,6 @@ import React from 'react';
 import classNames from 'classnames';
 import Typewriter from 'typewriter-effect';
 import './portfolio.css';
-import Welcome from './welcome';
 import About from './about';
 import Experience from './experience';
 import Resume from './resume';
@@ -22,146 +21,143 @@ class Portfolio extends React.Component {
       };
   }
 
-    changeTheme = () => {
-      const { theme } = this.state;
+  changeTheme = () => {
+    const { theme } = this.state;
 
-      if (theme === 'light') {
-          this.setState({ theme: 'dark' });
-      } else {
-          this.setState({ theme: 'light' });
-      }
-    };
+    if (theme === 'light') {
+        this.setState({ theme: 'dark' });
+    } else {
+        this.setState({ theme: 'light' });
+    }
+  };
 
-    changeView = (e) => {
-      this.setState({ activeView: e.target.value });
-    };
+  changeView = (e) => {
+    this.setState({ activeView: e.target.value });
+  };
 
-    onSubmit = (e) => {
-      e.preventDefault();
-      this.setState({ formSubmitted: true });
-    };
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.setState({ formSubmitted: true });
+  };
 
-    handleChange = (e) => {
-      this.setState({ visitorName: e.target.value });
-    };
+  handleChange = (e) => {
+    this.setState({ visitorName: e.target.value });
+  };
 
-    form = () => {
-      const { visitorName } = this.state;
+  welcome = () => {
+    const { visitorName, formSubmitted } = this.state;
 
-      return (
+    return (
+      <div id="welcome">
+        <Typewriter
+          onInit={(typewriter) => {
+            typewriter.typeString('Hello. ')
+              .pauseFor(500)
+              .start();
+            typewriter.typeString("I'm Andrew. ")
+              .pauseFor(500)
+              .start()
+            typewriter.typeString("What's your name?")
+              .pauseFor(500)
+              .start()
+          }}
+        />
         <form onSubmit={this.onSubmit}>
           <input type="text" value={visitorName} onChange={this.handleChange} />
           <input className="submit" type="submit" value="Introduce yourself" />
         </form>
-      );
-    };
-
-    welcome = () => {
-      const { visitorName, formSubmitted } = this.state;
-
-      return (
-        <div id="welcome">
+        {
+          formSubmitted
+          && (
           <Typewriter
             onInit={(typewriter) => {
-              typewriter.typeString('Hello. ')
-                .pauseFor(500)
+              typewriter.typeString(`Hello, ${visitorName}!`)
+                .pauseFor(1000)
                 .start();
-              typewriter.typeString("I'm Andrew. ")
-                .pauseFor(500)
-                .start()
-              typewriter.typeString("What's your name?")
-                .pauseFor(500)
-                .start()
             }}
           />
-          {this.form()}
+          )
+        }
+      </div>
+    );
+  };
+
+  renderHeader() {
+    const { theme, activeView } = this.state;
+
+    const changeThemeTo = theme === 'light' ? 'Light Theme' : 'Dark Theme';
+
+    return (
+      <div className="header">
+        <ul>
           {
-            formSubmitted
-            && (
-            <Typewriter
-              onInit={(typewriter) => {
-                typewriter.typeString(`Hello, ${visitorName}!`)
-                  .pauseFor(1000)
-                  .start();
-              }}
-            />
-            )
+            HEADER_OPTIONS.map((item, i) => (
+              // eslint-disable-next-line react/no-array-index-key
+              <li key={i}>
+                <button
+                  className={classNames('header-option', activeView === item ? 'active-view': 'hidden')}
+                  type="button"
+                  value={item}
+                  onClick={this.changeView}
+                >
+                  {item}
+                </button>
+              </li>
+            ))
           }
-        </div>
-      );
-    };
+        </ul>
+        <button
+          id="theme-button"
+          type="button"
+          onClick={this.changeTheme}
+        >
+          { changeThemeTo }
+        </button>
+      </div>
+    );
+  }
 
-    renderHeader() {
-      const { theme, activeView } = this.state;
+  renderView() {
+    const { activeView, visitorName } = this.state;
 
-      const changeThemeTo = theme === 'light' ? 'Light Theme' : 'Dark Theme';
+    this.setState({
+      visitorName: '',
+      formSubmitted: false
+    })
 
-      return (
-        <div className="header">
-          <ul>
-            {
-              HEADER_OPTIONS.map((item, i) => (
-                // eslint-disable-next-line react/no-array-index-key
-                <li key={i}>
-                  <button
-                    className={classNames('header-option', activeView === item ? 'active-view': 'hidden')}
-                    type="button"
-                    value={item}
-                    onClick={this.changeView}
-                  >
-                    {item}
-                  </button>
-                </li>
-              ))
-            }
-          </ul>
-          <button
-            id="theme-button"
-            type="button"
-            onClick={this.changeTheme}
-          >
-            { changeThemeTo }
-          </button>
-        </div>
-      );
+    let view;
+
+    switch(activeView) {
+      case 'About':
+        view = <About visitor={visitorName} />;
+        break;
+      case 'Experience':
+        view = <Experience />;
+        break;
+      case 'Resume':
+        view = <Resume />;
+        break;
+      case 'Contact':
+        view = <Contact />;
+        break;
+      default:
+        view = this.welcome();
+        break;
     }
 
-    renderView() {
-      const { activeView } = this.state;
+    return view;
+  }
 
-      let view;
+  render() {
+    const { theme } = this.state;
 
-      switch(activeView) {
-        case 'About':
-          view = <About />;
-          break;
-        case 'Experience':
-          view = <Experience />;
-          break;
-        case 'Resume':
-          view = <Resume />;
-          break;
-        case 'Contact':
-          view = <Contact />;
-          break;
-        default:
-          view = this.welcome();
-          break;
-      }
-
-      return view;
-    }
-
-    render() {
-      const { theme } = this.state;
-
-      return (
-        <div className={classNames('portfolio', theme)}>
-          {this.renderHeader()}
-          {this.renderView()}
-        </div>
-      );
-    }
+    return (
+      <div className={classNames('portfolio', theme)}>
+        {this.renderHeader()}
+        {this.renderView()}
+      </div>
+    );
+  }
 }
 
 export default Portfolio;
