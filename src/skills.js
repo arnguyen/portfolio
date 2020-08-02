39751147381553ee1359d04d0aa5be6ec17fdbdc
Andrew <ngandrew@umich.edu>
2020-08-02 17:26:44 -0400

    updates

--- src/skills.js
+++ src/skills.js
@@ -98,6 +98,84 @@ class Skills extends React.Component {
                 />
               </p>
             </div>
+            <div className="software-tools">
+              <h2>Software Tools</h2>
+              <p className="col-1">
+                Git
+                <ReactStars
+                  count={5}
+                  value={3}
+                  size={24}
+                  activeColor="#ffd700"
+                  edit={false}
+                />
+                Visual Studio
+                <ReactStars
+                  count={5}
+                  value={2}
+                  size={24}
+                  activeColor="#ffd700"
+                  edit={false}
+                />
+                VS Code
+                <ReactStars
+                  count={5}
+                  value={3}
+                  size={24}
+                  activeColor="#ffd700"
+                  edit={false}
+                />
+              </p>
+              <p className="col-2">
+                Chrome Developer Tools
+                <ReactStars
+                  count={5}
+                  value={3}
+                  size={24}
+                  activeColor="#ffd700"
+                  edit={false}
+                />
+                Atlassian/Jira
+                <ReactStars
+                  count={5}
+                  value={3}
+                  size={24}
+                  activeColor="#ffd700"
+                  edit={false}
+                />
+              </p>
+            </div>
+            <div className="languages">
+              <h2>Languages</h2>
+              <p className="col-1">
+                English
+                <ReactStars
+                  count={5}
+                  value={5}
+                  size={24}
+                  activeColor="#ffd700"
+                  edit={false}
+                />
+                Cantonese
+                <ReactStars
+                  count={5}
+                  value={2}
+                  size={24}
+                  activeColor="#ffd700"
+                  edit={false}
+                />
+              </p>
+              <p className="col-2">
+                French
+                <ReactStars
+                  count={5}
+                  value={2}
+                  size={24}
+                  activeColor="#ffd700"
+                  edit={false}
+                />
+              </p>
+            </div>
           </div> 
         )
     }
