859d7b85e898fc40911102dc6b5b8b20580d8ac4
Andrew <ngandrew@umich.edu>
2020-08-02 14:35:57 -0400

    updates

--- src/resume.js
+++ src/resume.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './resume.css';
 
 class Resume extends React.Component {
     constructor(props) {
@@ -12,7 +13,7 @@ class Resume extends React.Component {
           <div>
             <iframe src="Nguyen-Andrew-Resume.pdf" title="resume" />
             <p>
-              Also feel free to download my resume 
+              Also feel free to download my resume&nbsp;
               <a href="Nguyen-Andrew-Resume.pdf">
                 here
               </a>
